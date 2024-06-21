7ef3f2f762acbab6b5a78f182e722fcfbdd7acf3
Amr mohamed <amrmohamd229@gmail.com>
2024-06-21 20:07:56 +0300

    test

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image as keras_image
+from tensorflow import keras  # Updated import
 import numpy as np
 import io
 from PIL import Image
@@ -9,22 +8,21 @@ from PIL import Image
 app = FastAPI()
 
 # Load the saved model
-model = load_model("knee_xray_Xceptionnet_complete_model.h5")
+model = keras.models.load_model("knee_xray_Xceptionnet_complete_model.h5")
 
 @app.post("/predict/")
 async def predict(file: UploadFile = File(...)):
     contents = await file.read()
     img = Image.open(io.BytesIO(contents))
     
-
     img = img.resize((224, 224))
-    img_array = keras_image.img_to_array(img)
+    img_array = np.array(img)  # Convert image to numpy array
     img_array = np.expand_dims(img_array, axis=0)  # Create a batch
-    img_array /= 255.0  # Normalize
+    img_array = img_array / 255.0  # Normalize
     predictions = model.predict(img_array)
     predicted_class = np.argmax(predictions, axis=1)
     return JSONResponse(content={"predicted_label": int(predicted_class[0])})
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8004)
